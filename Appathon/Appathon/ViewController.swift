fb4e711060af3a15f11354c38168fb83614052a7
Josh Byster <joshb1050@gmail.com>
2015-05-30 09:35:25 -0500

    added project files

---
+++ Appathon/Appathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Appathon
+//
+//  Created by Josh Byster on 5/30/15.
+//  Copyright (c) 2015 Josh Byster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
